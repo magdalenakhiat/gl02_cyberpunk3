ebd3a222f6fd5198a3cf8ce8404cbeb17eb84f36
Gabriel Dosne <gabrieldosne@gmail.com>
2025-12-03 23:09:59 +0100

    Ajout classe Question

---
+++ src/Question.js
@@ -0,0 +1,12 @@
+var Question(enonce, type, matiere, auteur, reponses, reponsesCorrectes) {
+  this.enonce = enonce;
+  this.type = type;
+  this.matiere = matiere;
+  this.auteur = auteur;
+  this.reponses = reponses;
+  this.reponsesCorrectes = reponsesCorrectes;
+}
+
+Question.prototype.estEgale = function (question) {
+	return (this.enonce === question.enonce && this.type === question.type);
+}
