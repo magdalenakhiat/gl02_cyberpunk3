54ff8206c6c5ede6afdfc12e4098d15885a56e0e
Gabriel Dosne <gabrieldosne@gmail.com>
2025-12-04 23:11:07 +0100

    Refactorisation test parser GIFT

--- spec/giftParser_spec.js
+++ spec/giftParser_spec.js
@@ -5,25 +5,22 @@ describe("Suite de tests pour le parser de GIFT", function () {
     var testText2 = testText1 + "Q2:Quel est la capitale de l'Italie ?{~Londres;~Paris;=Rome;~Berlin}\n";
     
     var gParser = new GIFTParser();
-    describe("test", function () {
+    it("Parser un GIFT bien formée correctement", function () {
+        gParser.parse(testText2);
+        let testEnonc = gParser.parsedQ[0].enonce;
+        let testRep = gParser.parsedQ[0].reponses;
+        let testRepCorr = gParser.parsedQ[0].reponsesCorrectes;
         
-        it("Parser un GIFT bien formée correctement", function () {
-            gParser.parse(testText2);
-            let testEnonc = gParser.parsedQ[0].enonce;
-            let testRep = gParser.parsedQ[0].reponses;
-            let testRepCorr = gParser.parsedQ[0].reponsesCorrectes;
-            
-            expect(testEnonc).toEqual("Quel est la capitale de la France ?");
-            expect(testRep).toEqual(["Londres", "Paris", "Berlin", "Rome"]);
-            expect(testRepCorr).toEqual(["Paris"]);
-            
-            testEnonc = gParser.parsedQ[1].enonce;
-            testRep = gParser.parsedQ[1].reponses;
-            testRepCorr = gParser.parsedQ[1].reponsesCorrectes;
-            
-            expect(testEnonc).toEqual("Quel est la capitale de l'Italie ?");
-            expect(testRep).toEqual(["Londres", "Paris", "Rome", "Berlin"]);
-            expect(testRepCorr).toEqual(["Rome"]);
-        });
+        expect(testEnonc).toEqual("Quel est la capitale de la France ?");
+        expect(testRep).toEqual(["Londres", "Paris", "Berlin", "Rome"]);
+        expect(testRepCorr).toEqual(["Paris"]);
+        
+        testEnonc = gParser.parsedQ[1].enonce;
+        testRep = gParser.parsedQ[1].reponses;
+        testRepCorr = gParser.parsedQ[1].reponsesCorrectes;
+        
+        expect(testEnonc).toEqual("Quel est la capitale de l'Italie ?");
+        expect(testRep).toEqual(["Londres", "Paris", "Rome", "Berlin"]);
+        expect(testRepCorr).toEqual(["Rome"]);
     });
 });
