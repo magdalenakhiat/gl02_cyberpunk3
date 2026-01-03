39124c4b9a36b71fda8c62808fba9666567e0b11
Magdalena <magdalena.khiat@utt.fr>
2026-01-03 17:20:13 +0100

    Début de l'implémentation de simuler (chargement de l'examen avec le parser)

--- src/action/simuler.menu.action.js
+++ src/action/simuler.menu.action.js
@@ -1,7 +1,21 @@
+import fs from 'fs';
 import MenuAction from './MenuAction.js';
+import GiftParser from '../gift_parser.js';
 
 export default class SimulerMenuAction extends MenuAction {
     static id = 5;
     static label = 'Simuler un examen';
 
+    static async execute(rl, question) {
+        console.log('\n=== Simulation d’examen (V1) ===');
+
+        const filePath = await question('Chemin complet du fichier .gift : ');
+        const data = fs.readFileSync(filePath, 'utf-8');
+
+        const parser = new GiftParser();
+        parser.parse(data);
+
+        console.log('\nExamen chargé : ' + parser.parsedQ.length + ' question(s).');
+        console.log('Simulation non implémentée (V1).');
+    }
 }
